79bce488668807fb4a70dc6a331ee732c005913c
Takashi Yokoyama <6673423+Acrylic1322@users.noreply.github.com>
2018-10-27 17:32:45 +0900

    Add lambda_handler

--- main.py
+++ main.py
@@ -14,39 +14,50 @@ SLACK_PROFILE_EMOJI = ":bento:"
 SLACK_PREFIX_TEXT = "今日のお昼ご飯はこの中から選んでみては？\n"
 SLACK_SUFFIX_TEXT = ""
 
-# Setting stdin/out/err encoding
-sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding=ENCODING)
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding=ENCODING)
-sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding=ENCODING)
+def lambda_handler(event, context):
+    response = suggest_lunch()
 
-# Loading list from file
-list = []
-with open(LIST_FILE_NAME, "r", encoding=ENCODING) as list_file:
-    file_text = list_file.read()
-    list = json.loads(file_text)
+    return {
+        "statusCode": 200,
+        "body": json.dumps(response)
+    }
 
-# Picking items in the list randomly
-suggestions = []
-for i in range(NUM_SUGGESTION):
-    index = random.randrange(len(list))
-    suggestions.append(list[index])
-    del list[index]
+def suggest_lunch():
+    # Setting stdin/out/err encoding
+    sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding=ENCODING)
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding=ENCODING)
+    sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding=ENCODING)
 
-# Creating message
-message = SLACK_PREFIX_TEXT
-for i, item in enumerate(suggestions):
-    index = str(i + 1)
-    message += index + ". " + item + "\n"
-message += SLACK_SUFFIX_TEXT
+    # Loading list from file
+    list = []
+    with open(LIST_FILE_NAME, "r", encoding=ENCODING) as list_file:
+        file_text = list_file.read()
+        list = json.loads(file_text)
 
-# Posting to slack
-to_send_data = {
-    "username": SLACK_USER_NAME,
-    "icon_emoji": SLACK_PROFILE_EMOJI,
-    "text": message,
-}
-to_send_text = "payload=" + json.dumps(to_send_data)
-request = urllib.request.Request(WEB_HOOK_URL, data=to_send_text.encode(ENCODING), method="POST")
-with urllib.request.urlopen(request) as response:
-    response_body = response.read().decode(ENCODING)
-    print(response_body)
+    # Picking items in the list randomly
+    suggestions = []
+    for i in range(NUM_SUGGESTION):
+        index = random.randrange(len(list))
+        suggestions.append(list[index])
+        del list[index]
+
+    # Creating message
+    message = SLACK_PREFIX_TEXT
+    for i, item in enumerate(suggestions):
+        index = str(i + 1)
+        message += index + ". " + item + "\n"
+    message += SLACK_SUFFIX_TEXT
+
+    # Posting to slack
+    to_send_data = {
+        "username": SLACK_USER_NAME,
+        "icon_emoji": SLACK_PROFILE_EMOJI,
+        "text": message,
+    }
+    to_send_text = "payload=" + json.dumps(to_send_data)
+    request = urllib.request.Request(WEB_HOOK_URL, data=to_send_text.encode(ENCODING), method="POST")
+    response_body = ""
+    with urllib.request.urlopen(request) as response:
+        response_body = response.read().decode(ENCODING)
+
+    return response_body
