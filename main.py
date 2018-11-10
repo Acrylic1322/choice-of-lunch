948db5464358715f08d8c1806115afa0719b6d37
Takashi Yokoyama <6673423+Acrylic1322@users.noreply.github.com>
2018-11-10 15:21:12 +0900

    Fix styles according to pep.

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import csv
-import io
 import json
 import os
 import random
-import sys
 import urllib.request
 import datetime
 
 ENCODING = "utf-8"
 FILE_NAME_LIST = "./list.json"
-FILE_NAME_HOLIDAY="./syukujitsu_kyujitsu.csv"
+FILE_NAME_HOLIDAY = "./syukujitsu_kyujitsu.csv"
 NUM_SUGGESTION = 3
 SLACK_WEB_HOOK_URL = os.environ["WEB_HOOK_URL"]
 SLACK_USER_NAME = "お昼ごはん推薦"
@@ -17,35 +15,44 @@ SLACK_PROFILE_EMOJI = ":bento:"
 SLACK_PREFIX_TEXT = "今日のお昼ご飯はこの中から選んでみては？\n"
 SLACK_SUFFIX_TEXT = ""
 
-def lambda_handler(event, context):
-    if is_today_holiday() is False:
-        response = suggest_lunch()
 
+def lambda_handler(event, context):
+    """
+    This function will be called at first like main().
+    """
+    if is_today_holiday():
         return {
-            "statusCode": 200,
-            "body": json.dumps(response)
+            "statusCode": 204,
+            "body": "Not posted to slack, because today is holiday."
         }
 
+    response = suggest_lunch()
+
     return {
-        "statusCode": 204,
-        "body": "Not posted to slack, because today is holiday."
+        "statusCode": 200,
+        "body": json.dumps(response)
     }
 
+
 def is_today_holiday():
+    """
+    if today is holiday, return True.
+    """
     # Getting today and weekday
-    JST = datetime.timezone(datetime.timedelta(hours=+9), "JST")
-    now = datetime.datetime.now(JST)
+    jst = datetime.timezone(datetime.timedelta(hours=+9), "JST")
+    now = datetime.datetime.now(jst)
     today = now.strftime("%Y-%m-%d")
     weekday = now.weekday()
 
     # On Suturday or Sunday
     if weekday >= 5:
-      return True
+        return True
 
     # Judgement whether today is holiday
     with open(FILE_NAME_HOLIDAY, "r", encoding=ENCODING) as file_holiday:
         reader = csv.reader(file_holiday)
-        header = next(reader)
+        # Skip header
+        next(reader)
 
         for row in reader:
             if row[0] == today:
@@ -53,7 +60,12 @@ def is_today_holiday():
 
     return False
 
+
 def suggest_lunch():
+    """
+    Choice lunch store randomly.
+    Then, Post slack it.
+    """
     # Setting stdin/out/err encoding
     # Not use in aws lambda
     # sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding=ENCODING)
@@ -61,17 +73,16 @@ def suggest_lunch():
     # sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding=ENCODING)
 
     # Loading list from file
-    list = []
     with open(FILE_NAME_LIST, "r", encoding=ENCODING) as list_file:
         file_text = list_file.read()
-        list = json.loads(file_text)
+        store_list = json.loads(file_text)
 
     # Picking items in the list randomly
     suggestions = []
     for i in range(NUM_SUGGESTION):
-        index = random.randrange(len(list))
-        suggestions.append(list[index])
-        del list[index]
+        index = random.randrange(len(store_list))
+        suggestions.append(store_list[index])
+        del store_list[index]
 
     # Creating message
     message = SLACK_PREFIX_TEXT
@@ -87,7 +98,8 @@ def suggest_lunch():
         "text": message,
     }
     to_send_text = "payload=" + json.dumps(to_send_data)
-    request = urllib.request.Request(SLACK_WEB_HOOK_URL, data=to_send_text.encode(ENCODING), method="POST")
+    request = urllib.request.Request(
+        SLACK_WEB_HOOK_URL, data=to_send_text.encode(ENCODING), method="POST")
     response_body = ""
     with urllib.request.urlopen(request) as response:
         response_body = response.read().decode(ENCODING)
